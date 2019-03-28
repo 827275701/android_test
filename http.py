069da3b81991c4f03e4985296df5ab30218f710c
lhwei <827275701@qq.com>
2019-03-28 22:03:44 +0800

    update 2019/3/28

---
+++ http.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+# 导入 socket、sys 模块
+import socket
+import sys
+import test_recv
+
+# 创建 socket 对象
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+# 获取本地主机名
+host = socket.gethostname()
+
+port = 9999
+
+# 设置端口复用
+serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# 绑定端口号
+serversocket.bind((host, port))
+
+# 设置最大连接数，超过后排队
+serversocket.listen(5)
+
+while True:
+    # 建立客户端连接
+    clientsocket, addr = serversocket.accept()  
+
+    buf = clientsocket.recv(1024)
+    print(buf)
+
+    m = test_recv.recv(clientsocket)
+    m.test()
+
+    clientsocket.close()
+
+
+
+
+
+
+
+
+
+
---
+++ test_recv.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import socket
+
+class recv(object):
+    def __init__(self, sock):
+        self.msg = 'HTTP/1.1 200 OK\r\n\r\n'
+        self.sock = sock
+
+    def test(self):
+        self.msg += 'hello world\r\n'
+        self.sock.send(self.msg.encode('utf-8'))
+        
+    def login_yes(self):
+        self.msg += 'yes\r\n'
+        self.sock.send(self.msg.encode('utf-8'))
+
+    def login_no(self):
+        self.msg += 'no\r\n'
+        self.sock.send(self.msg.encode('utf-8'))
+
+    def now_data(self):
+        self.msg += 't_now:'
+        self.msg += 'h_now:'
+        self.msg += ''
+        self.sock.send(self.msg.encode('utf-8'))
+
